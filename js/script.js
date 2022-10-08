a7d8f5c189cc48dcfd3eedcff80577607f6bbd7f
jazchugomez125@gmail.com <jazchugomez125@gmail.com>
2022-10-08 00:05:16 -0300

    se terminó el trabajo

--- js/script.js
+++ js/script.js
@@ -9,3 +9,17 @@ function sumar(){
     }
 }
 //#endregion
+
+function sumando() {  
+    var x,y,suma,text;  
+    x = document.getElementById("num1").value;  
+    y = document.getElementById("num2").value;  
+    if (isNaN(x) || isNaN(y)) {  
+      text = "Es necesarios introducir dos números válidos";  
+    } else {  
+      //si no ponemos parseFloat concatenaría x con y  
+      suma=parseFloat(x)+parseFloat(y);  
+      text= suma;  
+    }  
+    document.getElementById("sumando").innerHTML = text;  
+  }  
