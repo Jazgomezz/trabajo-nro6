a8b7d27f2f3926ce53632228b46b9e130b3ef32f
jazchugomez125@gmail.com <jazchugomez125@gmail.com>
2022-10-07 23:32:06 -0300

    se agregó el formulario

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
+function muestraOpcion()  {
+    var opciones = document.getElementsByName("eleccion");
 
+    for(let i=0;i<opciones.length;i++); {
+        console.log(opciones[i]);
+        if(opciones[i].checked); {
+            alert(opciones[i].value);
+
+        }
+    }
+
+
+}
 
